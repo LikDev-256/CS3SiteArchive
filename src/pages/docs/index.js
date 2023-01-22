7b6c4c4314b7779555af2f8b7febc4bd1acc780f
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2023-01-22 11:00:19 +0100

    move docs to the new thing

--- src/pages/docs/index.js
+++ src/pages/docs/index.js
@@ -4,19 +4,33 @@ import Layout from "../../components/layout"
 import DocsCard from "../../components/cards/docs"
 import SEO from "../../components/seo"
 
-const DocsPage = () => {
+const links = [
+    {
+        "url": "https://docs.cloudstream.cf/",
+        "name": "Docs",
+        "desc": "You will find all the help and information here."
+    },
+    {
+        "url": "https://recloudstream.github.io/dokka/",
+        "name": "Dokka",
+        "desc": "Basically javadoc"
+    }
+]
 
+const DocsPage = () => {
     return <Layout>
         <div className="flex items-center w-full flex-col">
-            <article className="card bg-base-200 shadow-xl mx-10 mb-5 w-full md:w-2/3">
-                <div className="card-body">
-                    <h2 className="card-title">Dokka</h2>
-                    <p>Basically javadoc</p>
-                    <div className="card-actions justify-end">
-                        <a href="https://recloudstream.github.io/dokka/" className="btn btn-primary">View</a>
+            {links.map(it => (
+                <article className="card bg-base-200 shadow-xl mx-10 mb-5 w-full md:w-2/3" key={it.url}>
+                    <div className="card-body">
+                        <h2 className="card-title">{it.name}</h2>
+                        <p>{it.desc}</p>
+                        <div className="card-actions justify-end">
+                            <a href={it.url} className="btn btn-primary">View</a>
+                        </div>
                     </div>
-                </div>
-            </article>
+                </article>
+            ))}
             <StaticQuery
                 query={graphql`
                 query {
